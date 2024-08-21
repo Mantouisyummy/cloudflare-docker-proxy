2e45d9e933eb29d9cad37c8a55e3aa96d0e8fe30
Mantouisyummy <51238168+Mantouisyummy@users.noreply.github.com>
2024-08-21 20:21:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,7 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
+  "ghcr.mantouuwu.top": "https://ghcr.io",
   // staging
   "docker-staging.libcuda.so": dockerHub,
 };
